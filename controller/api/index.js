b4c6f591d0ff215da229126cd2b1d4eed1cd064c
RaiderNationBuilder <71417297+RaiderNationBuilder@users.noreply.github.com>
2021-12-01 22:18:31 -0600

    think heroku might be working maybe?

--- controller/api/index.js
+++ controller/api/index.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
-const apiRoutes = require('.');
-const homeRoutes = require('../home-routes.js');
-const dashboardRoutes = require('..dashboard-routes');
-router.use('/api', apiRoutes);
-router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes);
-router.use((req, res) => {
-    res.status(404).end();
-});
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
+
 module.exports = router;
