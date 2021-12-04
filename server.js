fcf813187cd4740efd7ee990dba75241b416f2d1
RaiderNationBuilder <71417297+RaiderNationBuilder@users.noreply.github.com>
2021-12-04 11:17:13 -0600

    update

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const routes = require('./controllers');
+const routes = require('./controller');
 const sequelize = require('./config/connection');
 const path = require('path');
-const helpers = require('./utils/helpers');
+const helpers = require('./utils/helper');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 const session = require('express-session');
